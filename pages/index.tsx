fe12508683fa72d0d37a8f44c2ceb4359fe70ac8
Ronny <ripunjoy.buddha@gmail.com>
2021-07-19 11:08:34 +0530

    configured sass with base abstract mixin files

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        
+          <h1>Hello we are making Spotify 🚀</h1>
       </main>
     </div>
   )
